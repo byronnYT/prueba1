697fed78fdcaa212544a5b53f1277da0522c6cec
byronnYT <38868773+byronnYT@users.noreply.github.com>
2022-10-18 10:44:39 +0200

    Add files via upload

---
+++ Examen/ExamenEj1/ExamenEj1/Module1.vb
@@ -0,0 +1,78 @@
+﻿Module Module1
+
+    Sub Main()
+        Const MAX_CANT_NUMEROS As Integer = 20
+        Dim valorIntroducido As String
+        Dim numero, contador, numMaximo, aleatorio, contadorMayorA50 As Integer
+        Dim numMinimo As Integer = Integer.MaxValue
+        Dim rnd As New Random
+
+        Console.WriteLine("EJERCICIO PARA ANALIZAR TU MEMORIA VISUAL Y CÁLCULO")
+        Console.WriteLine("")
+        Do
+            Console.Write($"Introduce la cantidad de números que debemos visualizar(1..{MAX_CANT_NUMEROS}): ")
+            valorIntroducido = Console.ReadLine()
+            If Not Integer.TryParse(valorIntroducido, numero) Then
+                Console.WriteLine("Debe ser un número entre 1 y 20")
+            End If
+        Loop Until numero > 1 AndAlso numero < 20
+
+        Console.WriteLine($"Observa detenidamente estos {numero} números: ")
+
+        Do
+            aleatorio = rnd.Next(1, 101)
+            Console.Write(aleatorio)
+            Console.Write(" ")
+            contador += 1
+
+            If aleatorio > numMaximo Then
+                numMaximo = aleatorio
+            ElseIf aleatorio < numMinimo Then
+                numMinimo = aleatorio
+            End If
+            If aleatorio > 50 Then
+                contadorMayorA50 += 1
+            End If
+        Loop Until contador = numero
+        Console.WriteLine("")
+        Console.WriteLine("Pulsa para continuar...")
+        Console.ReadKey()
+        Console.Clear()
+
+        Do
+            Console.WriteLine("¿Cuál era el mayor de todos ellos? ")
+            valorIntroducido = Console.ReadLine
+        Loop Until Integer.TryParse(valorIntroducido, numero) AndAlso numero >= 1 AndAlso numero <= 100
+
+        If numero = numMaximo Then
+            Console.WriteLine("Has acertado!")
+        Else
+            Console.WriteLine($"Lo siento, el número mayor era {numMaximo}")
+        End If
+
+        Do
+            Console.WriteLine("¿Cuál era el menor de todos ellos? ")
+            valorIntroducido = Console.ReadLine
+        Loop Until Integer.TryParse(valorIntroducido, numero) AndAlso numero >= 1 AndAlso numero <= 100
+
+        If numero = numMinimo Then
+            Console.WriteLine("Has acertado!")
+        Else
+            Console.WriteLine($"Lo siento, el número menor era {numMinimo}")
+        End If
+
+        Do
+            Console.WriteLine("¿Cuántos números había mayores que 50? ")
+            valorIntroducido = Console.ReadLine
+        Loop Until Integer.TryParse(valorIntroducido, numero) AndAlso numero >= 1 AndAlso numero <= 100
+
+        If numero = contadorMayorA50 Then
+            Console.WriteLine("Has acertado!")
+        Else
+            Console.WriteLine($"Lo siento, te has equivocado, había {contadorMayorA50} números mayores de 50")
+        End If
+
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Examen/ExamenEj2/ExamenEj2/Module1.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim letraStr, palabra As String
+        Dim contadorPalabras, contadorCaracteres As Integer
+        Dim letraChar As Char
+        Do
+            Console.Write("Introduce una letra: ")
+            letraStr = Console.ReadLine.ToUpper
+        Loop While letraStr.Length > 1
+        letraChar = letraStr
+
+        Console.WriteLine($"Debes introducir palabras que comiencen por {letraChar}")
+        Do
+            Console.Write($"Palabra que comienza por {letraChar} de mínimo 10 caracteres: ")
+            palabra = Console.ReadLine
+            contadorPalabras += 1
+            contadorCaracteres += palabra.Length
+        Loop Until contadorCaracteres <= 10 OrElse Not palabra.ToUpper.StartsWith(letraChar.ToString)
+        Console.WriteLine($"{palabra} no llega a 10 caracteres, ya no puedes introducir más palabras")
+        Console.WriteLine($"¡Bien!, has tecleado {contadorCaracteres} en {contadorPalabras}")
+        Console.WriteLine("Pulsa para finalizar...")
+        Console.ReadKey()
+        Select Case contadorCaracteres
+            Case 0
+                Console.WriteLine("No has introducido ninguna palabra válida")
+            Case 1 To 99
+                Console.WriteLine($"Solo has tecleado {contadorCaracteres} caracteres en {contadorPalabras} palabras")
+            Case 100 To 1000
+                Console.WriteLine($"¡Bien! {contadorCaracteres} caracteres en {contadorPalabras} palabras")
+            Case Else
+                Console.WriteLine($"¡¡¡Vaya dominio!!!, has tecleado {contadorCaracteres} caracteres en {contadorPalabras} palabras")
+        End Select
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
